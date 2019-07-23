15756f42616817481176deac3cb26af357396a58
Pablo Bello <thefactus@gmail.com>
2019-07-23 11:02:09 +0100

    Add graphql building type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,29 @@
+# frozen_string_literal: true
+
 module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World!"
+    # end
+
+    field :buildings, [Types::BuildingType], null: false
+
+    def buildings
+      Building.all
+    end
+
+    field :building, Types::BuildingType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def building(id:)
+      Building.find(id)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute'
+  end
+  post '/graphql', to: 'graphql#execute'
 end
